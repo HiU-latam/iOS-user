a71d5e04f720f007922410c768c040b0f49834eb
shiny solutions <shinysolutions16@gmail.comdsd>
2018-06-25 09:56:52 +0530

    Resolve issues

---
+++ Pods/Target Support Files/MaterialComponents/MaterialComponents-umbrella.h
@@ -0,0 +1,29 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "MDCFlexibleHeaderColorThemer.h"
+#import "MaterialFlexibleHeader.h"
+#import "MDCFlexibleHeaderContainerViewController.h"
+#import "MDCFlexibleHeaderView.h"
+#import "MDCFlexibleHeaderViewController.h"
+#import "MaterialThemes.h"
+#import "MDCColorScheme.h"
+#import "MDCTonalColorScheme.h"
+#import "MDCTonalPalette.h"
+#import "MaterialApplication.h"
+#import "UIApplication+AppExtensions.h"
+#import "MaterialUIMetrics.h"
+#import "MDCLayoutMetrics.h"
+
+FOUNDATION_EXPORT double MaterialComponentsVersionNumber;
+FOUNDATION_EXPORT const unsigned char MaterialComponentsVersionString[];
+
